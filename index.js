30e6bc567ce25cfeb0e01dda2cd874174349378b
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-07-12 22:48:40 -0500

    Agregando cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const conectarDB = require('./config/db');
-
+const cors = require('cors');
 
 const app = express();
 
 //Conectamos a la BD
 conectarDB();
+app.use(cors());
 
 app.use(express.json());
 
@@ -13,7 +14,7 @@ app.use('/api/usuarios', require('./routes/usuario'))
 
 
 app.listen(3000, () => {
-    console.log("Servidor corriendo exitosamente!");
+    console.log("Servidor corriendo exitosamente! puerto 3000");
 })
 
 app.get("/", (req, res) => {
