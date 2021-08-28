661a97f8dd4533934127ea0c713b179b17474849
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-08-28 02:53:40 -0500

    Poniendo uri repiado

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config( {path: 'variables.env'} );
+const mongoUri = "mongodb+srv://admin:admin123@cluster0.qaz2i.mongodb.net/meanusuario?retryWrites=true&w=majority"
 
 const conectarDB = async () => {
  
     try {
 
-        await mongoose.connect(process.env.DB_MONGO, {
+        // await mongoose.connect(process.env.DB_MONGO, {
+        await mongoose.connect(mongoUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
